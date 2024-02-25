c3fe048547a103bd7c8531df9db0f1ca05e36df8
Bek <rebecca.e.rootes@gmail.com>
2024-02-25 15:22:49 +0000

    deleted unwanted .gitignore file and added main guard to PIN.py

--- PIN/PIN.py
+++ PIN/PIN.py
@@ -2,8 +2,13 @@
 from PIN_functions import pin_verification
 
 correct_pin = 1984  # the correct PIN
-# calls pin_verification function with the correct PIN. Allows you to set pin_attempts.
-pin_verification(correct_pin, pin_attempts=3)
+
+def main():
+    # calls pin_verification function with the correct PIN. Allows you to set pin_attempts.
+    pin_verification(correct_pin, pin_attempts=3)
 
 # QUERY: should pin_attempts be set here or in the PIN_functions.py file?
 
+# Ensure the main function is executed only when the script is run directly, not when imported as a module
+if __name__ == "__main__":
+    main()
